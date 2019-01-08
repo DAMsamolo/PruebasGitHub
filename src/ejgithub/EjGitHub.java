924e16a2444b202aac3d02351251461025d4e2bf
dam <dam@172.26.219.101>
2019-01-08 09:14:09 +0100

    version 1.0

--- src/ejgithub/EjGitHub.java
+++ src/ejgithub/EjGitHub.java
@@ -15,7 +15,7 @@ public class EjGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Version 1.0");
+        System.out.println("Versión 1.0");
     }
     
 }
